e56b0a4d7de90904197d78f20a5e95902ea1cbb0
Sergey Koksharov <gentoosiastic@gmail.com>
2023-06-05 12:40:40 +0300

    feat: finish args.js task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,20 @@
 const parseArgs = () => {
-    // Write your code here 
+  const JOIN_SEPARATOR = ", ";
+  const args = process.argv.slice(2); // skip path to Node.js binary & path to executed script
+  const props = [];
+
+  let i = 0;
+
+  while (i < args.length) {
+    if (args[i].startsWith("--")) {
+      props.push(`${args[i].replace(/^--/, "")} is ${args[i + 1]}`);
+      i += 2;
+    } else {
+      i += 1;
+    }
+  }
+
+  console.log(props.join(JOIN_SEPARATOR));
 };
 
 parseArgs();
