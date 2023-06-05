183045059eabfff18df963c15533a13e7cc78201
Sergey Koksharov <gentoosiastic@gmail.com>
2023-06-05 11:48:24 +0300

    feat: complete list.js task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,22 @@
+import fsPromises from "node:fs/promises";
+import path from "node:path";
+import { getModuleDirectory } from "../utils/fs.js";
+
 const list = async () => {
-    // Write your code here 
+  const MODULE_DIRECTORY = getModuleDirectory(import.meta.url);
+  const FILES_DIR = "files";
+  const FILES_DIR_PATH = path.resolve(MODULE_DIRECTORY, FILES_DIR);
+  const ERROR_MESSAGE = "FS operation failed";
+
+  try {
+    const files = await fsPromises.readdir(FILES_DIR_PATH);
+
+    for (const file of files) {
+      console.log(file);
+    }
+  } catch {
+    throw new Error(ERROR_MESSAGE);
+  }
 };
 
 await list();
