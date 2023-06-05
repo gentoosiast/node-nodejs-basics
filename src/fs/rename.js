4cacb669f4e6cf4328c3475383f874b5df2266cf
Sergey Koksharov <gentoosiastic@gmail.com>
2023-06-05 11:36:10 +0300

    feat: complete rename.js task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,43 @@
+import fsPromises from "node:fs/promises";
+import path from "node:path";
+import { getModuleDirectory } from "../utils/fs.js";
+
+const isFileExists = async (path) => {
+  try {
+    await fsPromises.access(path);
+    return true;
+  } catch {
+    return false;
+  }
+};
+
 const rename = async () => {
-    // Write your code here 
+  const MODULE_DIRECTORY = getModuleDirectory(import.meta.url);
+  const FILES_DIRECTORY = "files";
+  const WRONG_FILENAME = "wrongFilename.txt";
+  const PROPER_FILENAME = "properFilename.md";
+  const WRONG_FILE = path.resolve(
+    MODULE_DIRECTORY,
+    FILES_DIRECTORY,
+    WRONG_FILENAME
+  );
+  const PROPER_FILE = path.resolve(
+    MODULE_DIRECTORY,
+    FILES_DIRECTORY,
+    PROPER_FILENAME
+  );
+  const ERROR_MESSAGE = "FS operation failed";
+
+  try {
+    const isDestFileExists = await isFileExists(PROPER_FILE);
+    if (isDestFileExists) {
+      throw new Error();
+    }
+
+    await fsPromises.rename(WRONG_FILE, PROPER_FILE);
+  } catch {
+    throw new Error(ERROR_MESSAGE);
+  }
 };
 
 await rename();
