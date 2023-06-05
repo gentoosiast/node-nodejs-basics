57a6c1533866cc4aacb40d904e5a79e3d7e6cf19
Sergey Koksharov <gentoosiastic@gmail.com>
2023-06-05 12:06:09 +0300

    feat: implement env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
 const parseEnv = () => {
-    // Write your code here 
+  const RSS_PREFIX = "RSS_";
+  const JOIN_SEPARATOR = "; ";
+  const rssVars = [];
+
+  for (const [envName, envValue] of Object.entries(process.env)) {
+    if (envName.startsWith(RSS_PREFIX)) {
+      rssVars.push(`${envName}=${envValue}`);
+    }
+  }
+
+  console.log(rssVars.join(JOIN_SEPARATOR));
 };
 
 parseEnv();
