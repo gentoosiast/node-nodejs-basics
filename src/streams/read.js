6e5a6cdd45a2467074cec24d40c094a2f0666d85
Sergey Koksharov <gentoosiastic@gmail.com>
2023-06-07 13:07:05 +0300

    feat: finish streams/read.js task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,21 @@
+import fsPromises from "node:fs/promises";
+import path from "node:path";
+import { getModuleDirectory } from "../utils/fs.js";
+
 const read = async () => {
-    // Write your code here 
+  const MODULE_DIRECTORY = getModuleDirectory(import.meta.url);
+  const FILES_DIR = "files";
+  const FILENAME = "fileToRead.txt";
+  const FILE = path.resolve(MODULE_DIRECTORY, FILES_DIR, FILENAME);
+  const ERROR_MESSAGE = "FS operation failed";
+
+  try {
+    const fh = await fsPromises.open(FILE);
+    const rs = fh.createReadStream();
+    rs.pipe(process.stdout);
+  } catch {
+    throw new Error(ERROR_MESSAGE);
+  }
 };
 
 await read();
