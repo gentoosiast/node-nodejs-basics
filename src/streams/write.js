57e8ebda3de7f3d7fbacdeeaa86dda52c838b324
Sergey Koksharov <gentoosiastic@gmail.com>
2023-06-07 13:44:52 +0300

    feat: implement streams/write.js task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,22 @@
+import fsPromises from "node:fs/promises";
+import path from "node:path";
+import { getModuleDirectory } from "../utils/fs.js";
+
 const write = async () => {
-    // Write your code here 
+  const MODULE_DIRECTORY = getModuleDirectory(import.meta.url);
+  const FILES_DIR = "files";
+  const FILENAME = "fileToWrite.txt";
+  const FILE = path.resolve(MODULE_DIRECTORY, FILES_DIR, FILENAME);
+  const ERROR_MESSAGE = "FS operation failed";
+
+  try {
+    const fh = await fsPromises.open(FILE, "w");
+    const ws = fh.createWriteStream();
+
+    process.stdin.pipe(ws);
+  } catch {
+    throw new Error(ERROR_MESSAGE);
+  }
 };
 
 await write();
