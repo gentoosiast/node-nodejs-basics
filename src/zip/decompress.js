d0d5fcccf2c94c5481da1166f3489ddb324e291a
Sergey Koksharov <gentoosiastic@gmail.com>
2023-06-07 15:28:19 +0300

    feat: complete decompress.js task

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,34 @@
+import fsPromises from "node:fs/promises";
+import path from "node:path";
+import { pipeline } from "node:stream";
+import zlib from "node:zlib";
+import { getModuleDirectory } from "../utils/fs.js";
+
 const decompress = async () => {
-    // Write your code here 
+  const MODULE_DIRECTORY = getModuleDirectory(import.meta.url);
+  const FILES_DIR = "files";
+  const SRC_FILENAME = "archive.gz";
+  const DEST_FILENAME = "fileToCompress.txt";
+  const SRC_FILE = path.resolve(MODULE_DIRECTORY, FILES_DIR, SRC_FILENAME);
+  const DEST_FILE = path.resolve(MODULE_DIRECTORY, FILES_DIR, DEST_FILENAME);
+  const ERROR_MESSAGE = "FS operation failed";
+
+  try {
+    const srcFh = await fsPromises.open(SRC_FILE);
+    const destFh = await fsPromises.open(DEST_FILE, "w");
+    const rs = srcFh.createReadStream();
+    const ws = destFh.createWriteStream();
+    const gunzip = zlib.createGunzip();
+
+    pipeline(rs, gunzip, ws, (err) => {
+      if (err) {
+        console.error(`An error occured: ${err}`);
+        process.exitCode = 1;
+      }
+    });
+  } catch {
+    throw new Error(ERROR_MESSAGE);
+  }
 };
 
 await decompress();
