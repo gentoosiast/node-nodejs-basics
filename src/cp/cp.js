c0078c421ddda0e32e50b6ec84f710eb4e706c54
Sergey Koksharov <gentoosiastic@gmail.com>
2023-06-09 19:57:04 +0300

    feat: implement cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,23 @@
+import { fork } from "node:child_process";
+import path from "node:path";
+import { getModuleDirectory } from "../utils/fs.js";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const MODULE_DIRECTORY = getModuleDirectory(import.meta.url);
+  const FILES_DIR = "files";
+  const SCRIPT_FILENAME = "script.js";
+  const SCRIPT_PATH = path.resolve(
+    MODULE_DIRECTORY,
+    FILES_DIR,
+    SCRIPT_FILENAME
+  );
+
+  const child = fork(SCRIPT_PATH, args);
+
+  child.on("error", (err) => {
+    console.error(`An error occured: ${err}`);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([42, "foo", { id: 1 }]);
