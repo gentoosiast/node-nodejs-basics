f44abc920955df2cf84cf054d92e4114bd7c56ce
Sergey Koksharov <gentoosiastic@gmail.com>
2023-06-17 18:09:01 +0300

    fix: switch to spawn() in cp.js to better match task requirements

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,19 +1,27 @@
-import { fork } from "node:child_process";
+import { spawn } from "node:child_process";
+import { pipeline } from "node:stream";
 import path from "node:path";
 import { getModuleDirectory } from "../utils/fs.js";
 
 const MODULE_DIRECTORY = getModuleDirectory(import.meta.url);
 const FILES_DIR = "files";
+const EXEC_COMMAND = "node";
 const SCRIPT_FILENAME = "script.js";
 const SCRIPT_PATH = path.resolve(MODULE_DIRECTORY, FILES_DIR, SCRIPT_FILENAME);
 
-const spawnChildProcess = async (args) => {
-  const child = fork(SCRIPT_PATH, args);
-
-  child.on("error", (err) => {
+const handleError = (err) => {
+  if (err) {
     console.error(`An error occured: ${err}`);
-  });
+    process.exitCode = 1;
+  }
+};
+
+const spawnChildProcess = async (args) => {
+  const child = spawn(EXEC_COMMAND, [SCRIPT_PATH, ...args]);
+
+  pipeline(process.stdin, child.stdin, handleError);
+  pipeline(child.stdout, process.stdout, handleError);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess([42, "foo", { id: 1 }]);
+spawnChildProcess([42, "foo", 28, "bar"]);
